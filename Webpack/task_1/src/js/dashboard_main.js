d2e00ceb0ea679f697769541a0e184621e87fa99
MiguelBG12 <miguel.bg12@gmail.com>
2023-11-11 21:49:59 -0500

    Fix bug

--- Webpack/task_1/src/js/dashboard_main.js
+++ Webpack/task_1/src/js/dashboard_main.js
@@ -1,22 +1,22 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+$(function() {
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button>Click here to get started</button>');
+    $('body').append("<p id='count'></p>");
+    $('body').append('<p>Copyright - Holberton School</p>');
 
-// Initialize a variable 'count' with an initial value of 0.
-let count = 0;
+    // Define a click counter function.
+    function updateCounter() {
+        let count = 0;
+        return function() {
+            count += 1;
+            $('#count').text(`${count} clicks on the button`);
+        }
+    }
 
-// Define a function 'updateCounter' to increment the 'count' variable
-function updateCounter() {
-  count++;
-  // updates the text content of the paragraph with the id 'count'.
-  $("#count").html(`${count} clicks on the button`);
-};
-
-// Attach a debounced click event handler to the button element using jQuery.
-// The 'updateCounter' function is invoked with a debounce of 500 milliseconds.
-$('button').on('click', _.debounce(updateCounter, 500));
+    // Attach a debounced click event handler to the button, invoking the click counter.
+    $('button').on('click', _.debounce( updateCounter(), 500));
+});
