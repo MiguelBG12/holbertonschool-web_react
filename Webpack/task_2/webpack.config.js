b465c8c18bb8624915b4268871d0f568333f12e7
MiguelBG12 <miguel.bg12@gmail.com>
2023-11-12 00:00:39 -0500

    Places the counter text to the right of the button and makes it bold. Additionally, set the background image of the element with the id `#logo to the Holberton logo, adjusting its width and height

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
+      },
+    ],
+  },
+  performance: {
+    maxAssetSize: 500000,
+  },
+};
