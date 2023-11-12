fb9af2ecd6ff90bbbed68753ccb9b6e47cd77f49
MiguelBG12 <miguel.bg12@gmail.com>
2023-11-11 19:32:49 -0500

    Update locations folders and files

--- Webpack/task_1/src/js/dashboard_main.js
+++ Webpack/task_1/src/js/dashboard_main.js
@@ -1,23 +1,26 @@
-import $ from 'jquery';
+// Import necessary libraries
+import $ from 'jquery';  // Import jQuery library
+import _ from 'lodash';  // Import lodash library
 
-// Add elements to the body using jQuery
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button id="startBtn">Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
-
-// Counter variable
+// Initialize a counter variable to keep track of button clicks
 let count = 0;
 
-// Function to update the counter and display it
-const updateCounter = () => {
-  count += 1;
-  $('#count').text(`${count} clicks on the button`);
-};
+// Function to update the counter and display the count on the HTML page
+function updateCounter() {
+  count += 1;  // Increment the counter
+  $('#count').text(`${count} clicks on the button`);  // Update the count in the HTML
+}
 
-// Debounce function from lodash
-const debouncedUpdateCounter = _.debounce(updateCounter, 1000);
+// Execute the following code when the document is ready
+$(function() {
+  // Append different HTML elements to the body using jQuery
+  $('body').append('<p>Holberton Dashboard</p>');  // Paragraph element
+  $('body').append('<p>Dashboard data for the students</p>');  // Paragraph element
+  $('body').append('<button id="startBtn">Click here to get started</button>');  // Button element
+  $('body').append('<p id="count"></p>');  // Paragraph element with id 'count'
+  $('body').append('<p>Copyright - Holberton School</p>');  // Another paragraph element
 
-// Bind the debounced function to the click event of the button
-$('#startBtn').on('click', debouncedUpdateCounter);
+  // Bind the debounce function from lodash to the click event on the button
+  $('#startBtn').click(_.debounce(updateCounter, 1000));
+  // The above line ensures that the updateCounter function is not called more than once per second
+});
