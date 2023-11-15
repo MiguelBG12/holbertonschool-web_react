bbf5cf6523e3dd0bb2a592f7ab286d5d684ded15
MiguelBG12 <miguel.bg12@gmail.com>
2023-11-15 16:50:58 -0500

    Adds an optional 'experienceTeachingC' attribute to the 'Teacher' interface within the 'Subjects' namespace. Defines a class 'Cpp' that extends 'Subject' with methods for teacher requirements and availability

---
+++ Typescript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,25 @@
+namespace Subjects {
+    // Extends the Teacher interface within the Subjects namespace
+    export interface Teacher {
+      experienceTeachingC?: number; // Optional attribute for C++
+    }
+  
+    // Class 'Cpp' extending from 'Subject'
+    export class Cpp extends Subject {
+      // Method returning requirements for C++
+      getRequirements(): string {
+        return 'Here is the list of requirements for Cpp';
+      }
+  
+      // Method to check available C++ teacher
+      getAvailableTeacher(): string {
+        // Checks if the teacher has experience teaching C++
+        if (this.teacher.experienceTeachingC) {
+          return `Available Teacher: ${this.teacher.firstName}`;
+        } else {
+          return 'No available teacher';
+        }
+      }
+    }
+}
+  
