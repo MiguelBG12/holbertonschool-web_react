6cd35e01d6d97fd3ddb2020d8d79b18fdc082d6e
MiguelBG12 <miguel.bg12@gmail.com>
2023-11-15 16:47:25 -0500

    Within the 'Subjects' namespace, create a 'Subject' class with a 'teacher' attribute and a 'setTeacher' method to assign a teacher to the subject.

---
+++ Typescript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,12 @@
+// Defines the Subject class within the Subjects namespace
+namespace Subjects {
+    // Class representing a subject with a teacher
+    export class Subject {
+      teacher: Teacher; // Holds the teacher information
+  
+      // Method to set the teacher for the subject
+      setTeacher(teacher: Teacher): void {
+        this.teacher = teacher; // Assigns the provided teacher
+      }
+    }
+}
