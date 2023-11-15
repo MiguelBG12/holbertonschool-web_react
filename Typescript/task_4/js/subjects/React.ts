5617f14323c8b67ef1c449ab8a695b1c63fa2eaa
MiguelBG12 <miguel.bg12@gmail.com>
2023-11-15 16:51:37 -0500

    Extends the 'Teacher' interface in 'Subjects' with an optional 'experienceTeachingReact' attribute. Create a 'React' class in the 'Subjects' namespace with methods for requirements and master availability for React.

---
+++ Typescript/task_4/js/subjects/React.ts
@@ -0,0 +1,24 @@
+namespace Subjects {
+    // Extends the Teacher interface within the Subjects namespace
+    export interface Teacher {
+      experienceTeachingReact?: number; // Optional attribute for React
+    }
+  
+    // Class 'React' extending from 'Subject'
+    export class React extends Subject {
+      // Method returning requirements for React
+      getRequirements(): string {
+        return 'Here is the list of requirements for React';
+      }
+  
+      // Method to check available React teacher
+      getAvailableTeacher(): string {
+        // Checks if the teacher has experience teaching React
+        if (this.teacher.experienceTeachingReact) {
+          return `Available Teacher: ${this.teacher.firstName}`;
+        } else {
+          return 'No available teacher';
+        }
+      }
+    }
+}
