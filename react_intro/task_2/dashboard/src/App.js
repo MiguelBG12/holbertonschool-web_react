72b5b5cedf232b7e2c75850b316b0b0ec46be9f4
MiguelBG12 <miguel.bg12@gmail.com>
2023-11-18 19:22:53 -0500

    These changes add email and password fields, allowing the user to click on the labels to directly select the associated inputs. A button with the text "OK" has also been added.

---
+++ react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,40 @@
+import HolbertonLogo from './Holberton_Logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  const isIndex = true;
+
+  const selectInput = (id) => {
+    document.getElementById(id).focus(); // Function to focus the input when clicking on the label
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={HolbertonLogo} alt="Holberton-logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        {/* Add input and label for the email */}
+        <label htmlFor="email" onClick={() => selectInput('email')}>Email</label>
+        <input type="email" id="email" />
+
+        {/* Add input and label for password */}
+        <label htmlFor="password" onClick={() => selectInput('password')}>Password</label>
+        <input type="password" id="password" />
+
+        {/* Add the "OK" button */}
+        <button>OK</button>
+      </div>
+      <footer className="App-footer">
+        <p>{getFooterCopy(isIndex)}</p>
+        <p>Copyright {getFullYear()} - Holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
+
