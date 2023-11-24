8690ffc6e1351f8b79da71376d50cdbf7fc4b89a
MiguelBG12 <miguel.bg12@gmail.com>
2023-11-23 20:00:29 -0500

    App displays Header, Login and Footer in React using separate components.

---
+++ react_props/task_0/dashboard/src/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="App">
+        <Header />
+        <Login />
+        <Footer />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
