fd6d68f7e17ab88186533915c7b0da7140ea2b90
MiguelBG12 <miguel.bg12@gmail.com>
2023-11-23 20:03:06 -0500

    Footer component in React that displays copyright information and dynamic text using utility functions.

---
+++ react_props/task_0/dashboard/src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+import { getFullYear, getFooterCopy } from '../utils';
+
+const Footer = () => {
+  return (
+    <footer className="App-footer">
+      <p>Copyright {getFullYear()} - {getFooterCopy('isIndex')}</p>
+    </footer>
+  );
+};
+
+export default Footer;
