e3d0922a7aae1a787390ae60835c373e825264b6
MiguelBG12 <miguel.bg12@gmail.com>
2024-03-10 15:14:04 -0500

    Function to sum major credits

--- TypeScript/task_5/js/main.ts
+++ TypeScript/task_5/js/main.ts
@@ -7,3 +7,14 @@ interface MinorCredits {
   credits: number;
   brand: string;
 }
+
+function sumMajorCredits(
+  subject1: MajorCredits,
+  subject2: MajorCredits
+): MajorCredits {
+  const sum: MajorCredits = {
+    credits: subject1.credits + subject2.credits,
+    brand: "MajorCreditsSum",
+  };
+  return sum;
+}
