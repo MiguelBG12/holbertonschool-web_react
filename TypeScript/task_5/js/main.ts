1ea50e357a866cd785d91a76480eec469004443e
MiguelBG12 <miguel.bg12@gmail.com>
2024-03-10 15:16:00 -0500

    Adding variables

--- TypeScript/task_5/js/main.ts
+++ TypeScript/task_5/js/main.ts
@@ -19,10 +19,25 @@ function sumMajorCredits(
   return sum;
 }
 
-function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+function sumMinorCredits(
+  subject1: MinorCredits,
+  subject2: MinorCredits
+): MinorCredits {
   const sum: MinorCredits = {
     credits: subject1.credits + subject2.credits,
-    brand: 'MinorCreditsSum',
+    brand: "MinorCreditsSum",
   };
   return sum;
 }
+
+const majorSubject1: MajorCredits = { credits: 3, brand: "Math" };
+const majorSubject2: MajorCredits = { credits: 4, brand: "Physics" };
+
+const minorSubject1: MinorCredits = { credits: 2, brand: "History" };
+const minorSubject2: MinorCredits = { credits: 1, brand: "Art" };
+
+const resultMajor = sumMajorCredits(majorSubject1, majorSubject2);
+const resultMinor = sumMinorCredits(minorSubject1, minorSubject2);
+
+console.log(resultMajor);
+console.log(resultMinor);
